212671debf31b2892117b3323d14d2b5b58daf40
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-08-19 17:46:29 +0500

    Coming Soon Page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  // Optional: You can add more configurations here if needed
+};
 
 export default nextConfig;
